24398f3b1245e33c0e1a94e29d8330fc31bf2124
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-01-22 12:31:37 +0530

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 // Write your javascript code here
+const blockQuoteEl = document.querySelector("blockquote > p");
+
+const buttonEl = document.getElementById("btn");
+
+//https://api.adviceslip.com/advice
+
+const apiUrl = "https://api.adviceslip.com/advice";
+
+async function getAdviceData() {
+  try {
+    const response = await fetch(apiUrl);
+    const jsonData = await response.json();
+    return jsonData;
+    console.log(jsonData);
+  } catch (error) {
+    console.error("some errors", error);
+  }
+}
+
+const adviceData = await getAdviceData();
+console.log(adviceData);
+
+blockQuoteEl.textContent = adviceData.slip.advice;
